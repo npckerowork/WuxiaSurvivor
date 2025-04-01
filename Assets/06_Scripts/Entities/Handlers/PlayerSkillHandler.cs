e422daf8b108c6c4fa4efeac50463d41cd8e651f
LeeJiyoung0511 <cjstk7597@gmail.com>
2025-04-01 14:13:28 +0900

    [Feat] 수리검 스킬 구현

--- Assets/06_Scripts/Entities/Handlers/PlayerSkillHandler.cs
+++ Assets/06_Scripts/Entities/Handlers/PlayerSkillHandler.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public class PlayerSkillHandler : MonoBehaviour
 {
+    public Transform SkillPos;
 
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    public float GetSkillDirection()
+    {
+        return spriteRenderer.flipX ? -1f : 1f;
+    }
 }
