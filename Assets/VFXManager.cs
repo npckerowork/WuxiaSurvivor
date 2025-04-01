62d7ded2a803eff8baf746daed01ef34a4db45f0
kaffu <kaffu@naver.com>
2025-04-01 10:20:46 +0900

    [Feat] VFX Manager 추가

---
+++ Assets/VFXManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXManager : Singleton<VFXManager>
+{
+    protected override void Initialize()
+    {
+        SetDontDestroyOnLoad();
+
+
+    }
+}
