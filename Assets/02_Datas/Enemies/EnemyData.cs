d34e1d18fd3862c3dc2a53b4606dd0756acb8e41
SpartaCodingClub <spartacodingclub.kr@gmail.com>
2025-04-01 20:32:05 +0900

    [Chore] 에너미 이름 추가

--- Assets/02_Datas/Enemies/EnemyData.cs
+++ Assets/02_Datas/Enemies/EnemyData.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Enemy", menuName = "ScriptableObjects/Enemy")]
 public class EnemyData : EntityData
 {
+    public string Name;
+
     public float AttackDamage = 5.0f;
     public float AttackDelay = 1.0f;
     public float AttackRange = 1.0f;
