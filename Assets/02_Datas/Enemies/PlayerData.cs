e5e1a9f491d828b0ea823ef47c9b8d5d6f2d99e1
SpartaCodingClub <spartacodingclub.kr@gmail.com>
2025-04-02 20:52:53 +0900

    [Feat] 플레이어 업그레이드 수치 적용

--- Assets/02_Datas/Enemies/PlayerData.cs
+++ Assets/02_Datas/Enemies/PlayerData.cs
@@ -3,5 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Player", menuName = "ScriptableObjects/Player")]
 public class PlayerData : EntityData
 {
-
+    public PlayerData() : base()
+    {
+        MaxHP *= DataManager.Instance.UpgradeData[UpgradeType.HP];
+        MoveSpeed *= DataManager.Instance.UpgradeData[UpgradeType.MoveSpeed];
+    }
 }
