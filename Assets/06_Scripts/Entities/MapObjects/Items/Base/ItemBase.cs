5496aee48fc9f5c90d753b0586b68f682ddcc164
kaffu <kaffu@naver.com>
2025-04-02 09:54:23 +0900

    [Feat] 아이템 획득 사운드 적용

--- Assets/06_Scripts/Entities/MapObjects/Items/Base/ItemBase.cs
+++ Assets/06_Scripts/Entities/MapObjects/Items/Base/ItemBase.cs
@@ -5,6 +5,9 @@ public class ItemBase : Poolable
     protected virtual void ApplyEffect()
     {
         ResourceManager.Instance.Destroy(gameObject); //풀로 돌려놓기
+
+        AudioManager.Instance.sfxController.
+            PlayClip(SfxName.GetItem, transform.position);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
